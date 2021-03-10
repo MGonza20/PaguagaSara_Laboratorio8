9af556699c5af4af961a3b8f705ca49a3a86ce07
MGonza20 <pag20634@uvg.edu.gt>
2021-03-10 13:40:49 -0600

    nuevas adiciones
    Añadí a personaje en primera persona, script para identificar objeto (aún no funciona) y objetos a identificar

---
+++ PaguagaSara_Lab8/Assets/Identify.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Identify : MonoBehaviour
+{
+    public GameObject[] canvass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvass = GetComponents<GameObject>();
+        canvass[0].SetActive(false);
+        canvass[1].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray CameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInf;
+
+        if (Physics.Raycast(CameraRay, out hitInf))
+        {
+            if (hitInf.collider.CompareTag("box"))
+            {
+                canvass[0].SetActive(true);
+
+            }
+            if (hitInf.collider.CompareTag("globe"))
+            {
+                canvass[1].SetActive(true);
+            }
+            else
+            {
+                canvass[0].SetActive(false);
+                canvass[1].SetActive(false);
+            }
+        }
+    }
+
+
+}
