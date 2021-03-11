b1b4bed737d5e515618f0e84b5aad9b438a14491
MGonza20 <pag20634@uvg.edu.gt>
2021-03-10 23:08:47 -0600

    Update Identify.cs

--- PaguagaSara_Lab8/Assets/Identify.cs
+++ PaguagaSara_Lab8/Assets/Identify.cs
@@ -32,13 +32,22 @@ public class Identify : MonoBehaviour
             {
                 canvass[0].SetActive(true);
             }
-        }
-        else
-        {           
-            
+            else
+            {
                 canvass[0].SetActive(false);
                 canvass[1].SetActive(false);
+            }
         }
+        else if (Physics.Raycast(CameraRay, out hitInf))
+        {
+            if (hitInf.collider != !hitInf.collider.CompareTag("globe") || hitInf.collider != hitInf.collider.CompareTag("box"))
+                canvass[0].SetActive(false);
+                canvass[1].SetActive(false);
+
+        }
+
+
+
     }
 
 
