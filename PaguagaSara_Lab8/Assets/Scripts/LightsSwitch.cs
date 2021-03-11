e63d8dd2d68cfc361441f91978604ecccb8e816f
MGonza20 <pag20634@uvg.edu.gt>
2021-03-11 15:29:41 -0600

    añadí spotlight y cookie

---
+++ PaguagaSara_Lab8/Assets/Scripts/LightsSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsSwitch : MonoBehaviour
+{
+
+    //AudioSource flashlightSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //flashlightSFX = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        GetComponent<AudioSource>().Play();
+
+    }
+}
